64cc56b571306b8076a6625f12c4b036449e4d6b
nosru30 <106005051+nosru30@users.noreply.github.com>
2025-06-14 15:43:34 +0000

    ｎ

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -44,7 +44,7 @@ app.use(cors({
         'https://127.0.0.1:3000',
         'https://127.0.0.1:5173',
         process.env.CORS_ORIGIN,
-        process.env.FRONTEND_URL
+        process.env.VITE_FRONTEND_URL
       ].filter(Boolean)
     }
     
